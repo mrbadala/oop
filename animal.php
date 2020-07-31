8ba8b00efe992d920819ec9ca7727738fd4846bc
Taufikza <68920754+mrbadala@users.noreply.github.com>
2020-07-31 10:54:44 +0700

    Soal

---
+++ animal.php
@@ -0,0 +1,11 @@
+<?php
+$sheep = new Animal("shaun");
+
+echo $sheep->name; // "shaun"
+echo $sheep->legs; // 2
+echo $sheep->cold_blooded // false
+
+
+// NB: Boleh juga menggunakan method get (get_name(), get_legs(), get_cold_blooded())
+
+?>
